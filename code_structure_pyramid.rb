d7c716ac7fdfce0a22de694d22028ab2c4223c92
rishabh <rgupta@grepruby.com>
2017-08-23 19:32:10 +0530

    Initial code structure for pyramid pattern program

---
+++ code_structure_pyramid.rb
@@ -0,0 +1,28 @@
+class Pyramid
+
+  def initialize(r)
+	# initialize row 
+    @row = r
+  end
+   
+   # args : @row
+   # return left alligned pyramid
+   
+   def Left
+    # code to return left pyramid
+   end
+
+   # args : @row
+   # return right alligned pyramid
+   
+   def Right
+	# code to return right pyramid
+   end
+
+   # args : @row
+   # return center alligned pyramid
+   
+   def Center
+    # return center alligned pyramid
+   end
+end

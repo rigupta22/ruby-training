11ec33f053f7a52805b44edd5440d007d5945860
rishabh <rgupta@grepruby.com>
2017-08-29 14:09:04 +0530

    Implement Sales tax-calculator

---
+++ sales tax/code/tax_calculator.rb
@@ -0,0 +1,14 @@
+module TaxCalculator
+	def tax
+		rate= 0.0
+    rate += 0.05 if imported?
+   rate += 0.10 unless exempt?
+  (@price*rate*@quantity).round(2)
+  	
+	end
+
+	def total_price
+	total=((@quantity*@price) + tax).round(2)
+		
+	end
+end

b3d12e35e719a27e4f93eca7672aecb55078fff3
rishabh <rgupta@grepruby.com>
2017-09-13 20:39:01 +0530

    improved intendation for pyramid

--- test/tc_code_struct_pyramid.rb
+++ test/tc_code_struct_pyramid.rb
@@ -1,19 +1,16 @@
-require_relative "../code/code_structure_pyramid"
-require "test/unit"
- 
-class Test_code_structure_pyramid < Test::Unit::TestCase
- 
-  def test_left
-   assert_equal("*\n**\n***\n****\n*****\n", Pyramid.new(5).left )
+require_relative '../code/code_structure_pyramid'
+require 'test/unit'
 
+class Test_code_structure_pyramid < Test::Unit::TestCase
+  def test_left
+    assert_equal("*\n**\n***\n****\n*****\n", Pyramid.new(5).left)
   end
 
   def test_right
-  	assert_equal("    *\n   **\n  ***\n ****\n*****\n", Pyramid.new(5).right )
+    assert_equal("    *\n   **\n  ***\n ****\n*****\n", Pyramid.new(5).right)
   end
 
   def test_center
-  	assert_equal("    * \n   * * \n  * * * \n * * * * \n* * * * * \n", Pyramid.new(5).center )
+    assert_equal("    * \n   * * \n  * * * \n * * * * \n* * * * * \n", Pyramid.new(5).center)
   end
- 
 end
